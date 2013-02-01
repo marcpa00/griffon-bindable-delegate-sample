b17148b42193d2d0a0d0b97c0668d541ab9eaf96
Marc Paquette <marcpa@mac.com>
2013-02-01 13:55:50 -0500

    Ajout d'une classe de domaine

---
+++ src/main/domain/Person.groovy
@@ -0,0 +1,11 @@
+package domain
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: marcpa
+ * Date: 2013-02-01
+ * Time: 12:28
+ * To change this template use File | Settings | File Templates.
+ */
+class Person {
+}
